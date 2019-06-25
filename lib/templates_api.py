0ad10a103941b672d673d22eaed75667c2ef04bc
Craig Drummond <craig.drummond@roke.co.uk>
2019-06-25 18:38:07 +0100

    Adjust thumbnail size to screen width

--- lib/templates_api.py
+++ lib/templates_api.py
@@ -20,21 +20,5 @@ def root(path):
         abort(403)
         return
 
-    # render_template does not seem to set mimetype for css???
-    if path.endswith('.css'):
-        path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../templates/"+path)
-        if not os.path.exists(path):
-            abort(404)
-            return
-
-        with open(path, 'r') as temp:
-            data=temp.read()
-            data=data.replace("{{THUMB_SIZE}}", str(config.htmlThumbSize))
-            data+=".navbar { display: none; }"
-            return data, 200, {'Content-Type': 'text/css; charset=utf-8'}
-
-        abort(404)
-        return
     return render_template(path, SERVER_ROOT=config.serverRoot, DEF_BROWSE='browse')
-
 
